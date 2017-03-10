2e76d69d5584ec60cfca983dbd863424dfe603f2
preferencewl <wangleipianai@163.com>
2017-03-10 18:08:28 +0800

    php部分数组函数

--- array/array_combine.php
+++ array/array_combine.php
@@ -1,7 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 2017/3/10
- * Time: 17:39
- */
+//数组的组合  将一个数组的值作为键，另一个数组的值作为值
+//两个数组的元素个数必须是一样，否则会抛出异常
+
+$arr1 = array('a','b','c','d','e','f');
+$arr2 = array(1,2,3,4,5,6);
+$arr = array_combine($arr1,$arr2);
+echo '<pre>';
+print_r($arr);
--- array/array_diff_assoc.php
+++ array/array_diff_assoc.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 2017/3/10
- * Time: 17:52
- */
+//array_diff_assoc()函数 返回在第一个数组中出现的元素，不在其它数组中出现的元素  不仅对比值，还要对比数组的键
+$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
+$array2 = array("a" => "green", "yellow", "red");
+
+$arr = array_diff_assoc($array1,$array2);
+echo '<pre>';
+print_r($arr);
